2f2ec7f7b2950e35b545ed0ec4017f4be99d5e5b
danielgolabek <daniel.golabek@gmail.com>
2018-04-07 19:03:57 +0100

    Tenants validatoin

--- BlendedAdmin/Startup.cs
+++ BlendedAdmin/Startup.cs
@@ -39,7 +39,7 @@ namespace BlendedAdmin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            HostingOptions hostingOptions = Configuration.GetSection("HostingOptions").Get<HostingOptions>();
+            HostingOptions hostingOptions = Configuration.GetSection("Hosting").Get<HostingOptions>();
             DatabaseOptions databaseOptions = Configuration.GetSection("Database").Get<DatabaseOptions>();
 
             services.AddMemoryCache();
