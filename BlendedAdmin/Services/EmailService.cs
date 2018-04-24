179e16e230e25248a65aa4d777b8b78e9a4ae616
danielgolabek <daniel.golabek@gmail.com>
2018-04-24 21:39:22 +0100

    Mail Template

--- BlendedAdmin/Services/EmailService.cs
+++ BlendedAdmin/Services/EmailService.cs
@@ -53,25 +53,19 @@ namespace BlendedAdmin.Services
         }
 
         private string _mailTemplate = @"
-<!DOCTYPE html PUBLIC "" -//W3C//DTD XHTML 1.0 Transitional//EN"" ""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"">
-< html xmlns=""http://www.w3.org/1999/xhtml"">
-<head>
-    <meta name = ""viewport"" content=""width=device-width, initial-scale=1.0"" />
-    <meta http-equiv=""Content-Type"" content=""text/html; charset=UTF-8"" />
-    <title>{{title}}</title>
-    <style type = ""text/css"" rel=""stylesheet"" media=""all"">
-        h1 {
-            color: #2F3133;
-            font-size: 19px;
-            font-weight: bold;
-        }
-    </style>
-</head>
+<html>
+    <head>
+        <meta name = ""viewport"" content=""width=device-width, initial-scale=1.0"" />
+        <meta http-equiv=""Content-Type"" content=""text/html; charset=UTF-8"" />
+        <title>{{title}}</title>
+        <style type=""text/css"" rel=""stylesheet"" media=""all"">
+        </style>
+    </head>
     <body>
         <h1>Hi,</h1>
         <div>{{content}}</div>
-	    <div style = ""margin-top:20px; color:#6c757d"" > cheers,<br/>Blended Admin</div>
-        </body>
+	    <div style=""margin-top:20px;"">Cheers,<br/>Blended Admin</div>
+    </body>
 </html>";
     }
 }
